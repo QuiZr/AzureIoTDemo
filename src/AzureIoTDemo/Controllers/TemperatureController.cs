5f9c998fcaf1789e73fb39e5dd6b7b41d2213c14
QuiZr <quizr@outlook.com>
2016-12-08 20:32:21 +0100

    Added controller and set up dependency injection

---
+++ src/AzureIoTDemo/Controllers/TemperatureController.cs
@@ -0,0 +1,21 @@
+﻿using AzureIoTDemo.Data;
+using AzureIoTDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureIoTDemo.Controllers
+{
+    [Route("api/[controller]")]
+    public class TemperatureController : Controller
+    {
+        private TemperatureContext _context;
+        public TemperatureController(TemperatureContext context)
+        {
+            _context = context;
+        }
+    }
+}
