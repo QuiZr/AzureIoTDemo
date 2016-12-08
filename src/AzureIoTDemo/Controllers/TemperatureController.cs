e31c21a44e5f84421a290972efd5756d589758f4
QuiZr <quizr@outlook.com>
2016-12-08 21:14:38 +0100

    Implemented basic API calls

--- src/AzureIoTDemo/Controllers/TemperatureController.cs
+++ src/AzureIoTDemo/Controllers/TemperatureController.cs
@@ -17,5 +17,49 @@ namespace AzureIoTDemo.Controllers
         {
             _context = context;
         }
+
+        // GET api/temperature
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var tempReads = await _context
+                .TemperatureReads
+                .ToListAsync();
+
+            if (tempReads.Count == 0)
+                return NoContent();
+
+            return Ok(tempReads);
+        }
+
+        // GET api/temperature/{id}
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var tempRead = await _context
+                .TemperatureReads
+                .FirstOrDefaultAsync(item => item.ID == id);
+
+            if (tempRead == null)
+                return NotFound();
+
+            return Ok(tempRead);
+        }
+
+        // POST api/temperature
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] TemperatureRead tempRead)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            // Because binding doesn't seem to work.
+            tempRead.ID = 0;
+            _context.Add(tempRead);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }
