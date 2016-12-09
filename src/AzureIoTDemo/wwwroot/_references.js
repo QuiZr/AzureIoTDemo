20b630605c700b7270cc2825eb43f30569b28777
QuiZr <quizr@outlook.com>
2016-12-09 11:58:03 +0100

    Fixed Bower

--- src/AzureIoTDemo/wwwroot/_references.js
+++ src/AzureIoTDemo/wwwroot/_references.js
@@ -1,5 +1,5 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
-/// <reference path="lib/bootstrap/dist/js/npm.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
+/// <reference path="lib/tether/dist/js/tether.js" />
