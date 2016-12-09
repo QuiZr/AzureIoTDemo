f9064f0060299354e74d854663800c2892e640c3
QuiZr <quizr@outlook.com>
2016-12-09 11:27:07 +0100

    Added "chart" action and view

--- src/AzureIoTDemo/Controllers/TempController.cs
+++ src/AzureIoTDemo/Controllers/TempController.cs
@@ -21,5 +21,22 @@ namespace AzureIoTDemo.Controllers
             var tempReads = await _context.TemperatureReads.ToListAsync();
             return View(tempReads);
         }
+
+        // GET /Temp/Chart?place={place}
+        public async Task<IActionResult> Chart(string place)
+        {
+            if (string.IsNullOrEmpty(place))
+                return BadRequest();
+
+            var tempReads = await _context
+                .TemperatureReads
+                .Where(tempRead => tempRead.Place == place)
+                .ToListAsync();
+
+            if (tempReads.Count == 0)
+                return NotFound();
+
+            return View(tempReads);
+        }
     }
 }
